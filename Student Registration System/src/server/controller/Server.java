ce0691cd06bf8837258ae0c90dd1a8d58acd4e02
Joel Happ <joelhapp7@gmail.com>
2020-04-20 20:33:45 -0600

    Expand thread pool, add javadoc

--- Student Registration System/src/server/controller/Server.java
+++ Student Registration System/src/server/controller/Server.java
@@ -54,7 +54,7 @@ public class Server {
 		clientCount = 0;
 
 		//Make the thread pool
-		pool = Executors.newFixedThreadPool(2);
+		pool = Executors.newFixedThreadPool(10);
 
 		//Start the server socket
 		try
@@ -73,8 +73,8 @@ public class Server {
 
 		// You can comment & uncomment the following lines of code
 		// to switch between using the real database or the pseudo database.
-		db = new PseudoDB();
-		//db = new RealDB();
+		//db = new PseudoDB();
+		db = new RealDB();
 
 
 		catalogue = db.loadDatabase();
